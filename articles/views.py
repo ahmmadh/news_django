688c069bc15c2260b61a03f109452c13e2945c33
ahmmadh <aalhelali99@hotmail.com>
2025-04-09 15:00:53 +0300

    added authorization logic w/mixins and custom mixin

--- articles/views.py
+++ articles/views.py
@@ -3,17 +3,18 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
     template_name = "article_list.html"
 
-class ArticleDetailView(DetailView): # new
+class ArticleDetailView(LoginRequiredMixin,DetailView): # new
     model = Article
     template_name = "article_detail.html"
 
-class ArticleUpdateView(UpdateView): # new
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView): # new
     model = Article
     fields = (
     "title",
@@ -21,19 +22,29 @@ class ArticleUpdateView(UpdateView): # new
     )
     template_name = "article_edit.html"
 
-class ArticleDeleteView(DeleteView): # new
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView): # new
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
-class ArticleCreateView(CreateView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model = Article
-    template_name = "article_new"
+    template_name = "article_new.html"
     fields = (
         "title",
-        "body",
-        "author"
+        "body"
     )
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form) 
 
 
 # Create your views here.
